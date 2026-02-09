bc4c0049fe6ef7afebec6931e059c55a6027c97c
Howlrr <91945857+Howlrr@users.noreply.github.com>
2026-02-09 11:27:46 +0300

    Added saving Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         MarsStorage storage = new MarsStorage();
+        fileManager manager = new fileManager();
 
         storage.addResource("Iron", 300);
         storage.addResource("Water", 345);
@@ -8,16 +12,34 @@ public class Main {
 
         storage.printInventory();
 
+        manager.saveThis(storage);
+
         try {
             System.out.println("Попотка забрать 40 Iron...");
             storage.removeResource("Iron", 40);
 
             System.out.println("Попытка забрать 300 Iron...");
             storage.removeResource("Iron", 300);
-        }
-        catch (InsufficientResourcesException e) {
+        } catch (InsufficientResourcesException e) {
             System.out.println(e.getMessage());
         }
         storage.printInventory();
+    }
+    public static class fileManager {
+        private String fileName = "SavedMars.txt";
+
+        public void saveThis(MarsStorage storage) {
+            var data = storage.getInventory();
+
+            try (PrintWriter out = new PrintWriter((new FileWriter(fileName)))){
+                for (var entry : data.entrySet()){
+                    out.println(entry.getKey() + ":" + entry.getValue());
+                }
+            }
+
+            catch(IOException e){
+                System.out.println("Ошибка записи" + e.getMessage());
+            }
+        }
     }
 }
